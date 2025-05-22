2a6efe4c3ecc43273eb5c90429fde46ed9fa2621
Masum264 <masumh246@gmail.com>
2025-05-22 22:51:34 +0600

    Third commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import MainLayout from './layouts/MainLayout.jsx';
 import Home from './components/Home.jsx';
 import AddCoffee from './components/AddCoffee.jsx';
 import UpdateCoffee from './components/UpdateCoffee.jsx';
+import CoffeeDetails from './components/CoffeeDetails.jsx';
 
 const router = createBrowserRouter([
   {
@@ -32,7 +33,12 @@ const router = createBrowserRouter([
       },
 
       {
-        path: "UpdateCoffee",
+        path:"coffee/:id",
+        Component: CoffeeDetails
+      },
+
+      {
+        path: "UpdateCoffee/:id",
         Component: UpdateCoffee
       }
     ]
